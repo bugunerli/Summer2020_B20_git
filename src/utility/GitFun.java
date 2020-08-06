a215cdd3a44170a88cca7e4f99ff3174a512396e
bugunerli <69088589+bugunerli@users.noreply.github.com>
2020-08-05 19:32:50 -0600

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -5,6 +5,8 @@ public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
         // Practicing on GitHub class
+        //This line is from GitHub
+        //......changes I am practicing on GitHub.
 
 
     }
